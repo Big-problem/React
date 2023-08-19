57499927c36bccafe204614e1bd1564d15c38052
Big-problem <117372204+Big-problem@users.noreply.github.com>
2023-08-19 23:42:19 +0800

    React fun facts

--- react-JS/index.js
+++ react-JS/index.js
@@ -1,16 +1,15 @@
-// JSX
-const element = // 一次只能回傳一個parent
-  (
-    <nav>
-      <h1>React!</h1>
-      <ul>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <p className="header">This is a paragraph</p>
-    </nav>
-  );
-console.log(element); // We get a JS object
+const element = (
+  <div>
+    <img src="Image/react-logo.png" width="40px"></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Power thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
 
 ReactDOM.render(element, document.querySelector(".root"));
