8eaf8f0a51f4c2b3528d7cdb6eb9661cc64fad9e
Big-problem <117372204+Big-problem@users.noreply.github.com>
2023-08-19 23:02:44 +0800

    Learning JSX

--- react-JS/index.js
+++ react-JS/index.js
@@ -1,5 +1,16 @@
-function MainContent() {
-  return <h>So Cool~~</h>;
-}
+// JSX
+const element = // 一次只能回傳一個parent
+  (
+    <nav>
+      <h1>React!</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <p className="header">This is a paragraph</p>
+    </nav>
+  );
+console.log(element); // We get a JS object
 
-ReactDOM.render(<MainContent />, document.querySelector(".root"));
+ReactDOM.render(element, document.querySelector(".root"));
