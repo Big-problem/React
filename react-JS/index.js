59f6cf92eb5cd53fb415967cd851dc81e40234c8
Big-problem <117372204+Big-problem@users.noreply.github.com>
2023-08-20 10:12:49 +0800

    Use multiple components to build a page

--- react-JS/index.js
+++ react-JS/index.js
@@ -1,24 +1,18 @@
-// const element = (
-// <div>
-//   <img src="Image/react-logo.png" width="40px"></img>
-//   <h1>Fun facts about React</h1>
-//   <ul>
-//     <li>Was first released in 2013</li>
-//     <li>Was originally created by Jordan Walke</li>
-//     <li>Has well over 100K stars on Github</li>
-//     <li>Is maintained by Facebook</li>
-//     <li>Power thousands of enterprise apps, including mobile apps</li>
-//   </ul>
-// </div>
-// );
-
-// ReactDOM.render(element, document.querySelector(".root"));
-
 // Use function to create React component
-function ReactComponent() {
+// Component is a function that returns React elements
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="Image/react-logo.png" width="40px"></img>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
   return (
     <div>
-      <img src="Image/react-logo.png" width="40px"></img>
       <h1>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
@@ -31,4 +25,22 @@ function ReactComponent() {
   );
 }
 
-ReactDOM.render(<ReactComponent />, document.querySelector(".root"));
+function Footer() {
+  return (
+    <footer>
+      <small>© 2023 Brian development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.querySelector(".root"));
