faf227245f7526c270df7f188ba9c599c82f1981
Big-problem <117372204+Big-problem@users.noreply.github.com>
2023-08-20 09:55:45 +0800

    React component

--- react-JS/index.js
+++ react-JS/index.js
@@ -1,15 +1,34 @@
-const element = (
-  <div>
-    <img src="Image/react-logo.png" width="40px"></img>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Power thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+// const element = (
+// <div>
+//   <img src="Image/react-logo.png" width="40px"></img>
+//   <h1>Fun facts about React</h1>
+//   <ul>
+//     <li>Was first released in 2013</li>
+//     <li>Was originally created by Jordan Walke</li>
+//     <li>Has well over 100K stars on Github</li>
+//     <li>Is maintained by Facebook</li>
+//     <li>Power thousands of enterprise apps, including mobile apps</li>
+//   </ul>
+// </div>
+// );
 
-ReactDOM.render(element, document.querySelector(".root"));
+// ReactDOM.render(element, document.querySelector(".root"));
+
+// Use function to create React component
+function ReactComponent() {
+  return (
+    <div>
+      <img src="Image/react-logo.png" width="40px"></img>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Power thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<ReactComponent />, document.querySelector(".root"));
