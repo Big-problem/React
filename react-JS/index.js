36f06ae7f77b46be3e955cd44158ab29499348de
Big-problem <117372204+Big-problem@users.noreply.github.com>
2023-08-20 10:53:46 +0800

    style with css

--- react-JS/index.js
+++ react-JS/index.js
@@ -2,9 +2,14 @@
 // Component is a function that returns React elements
 function Header() {
   return (
-    <header>
-      <nav>
-        <img src="Image/react-logo.png" width="40px"></img>
+    <header className="header">
+      <nav className="nav-bar">
+        <img src="Image/react-logo.png" className="header-img"></img>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
     </header>
   );
@@ -14,20 +19,20 @@ function MainContent() {
   return (
     <div>
       <h1>Fun facts about React</h1>
-      <ul>
+      <ol className="content-list">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Power thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      </ol>
     </div>
   );
 }
 
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <small>© 2023 Brian development. All rights reserved.</small>
     </footer>
   );
